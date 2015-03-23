b6f87d9eb310a09c93984e05898042590212c899
Doug Goldstein <doug.goldstein@synapse-wireless.com>
2015-03-23 16:45:58 -0500

    Add run-recovery script to synapse-recover pkg
    Added the script that does most of the heavy lifting to the
    synapse-recover package.

--- recipes-core/synapse-recovery/synapse-recovery.bb
+++ recipes-core/synapse-recovery/synapse-recovery.bb
@@ -1,31 +1,26 @@
-SUMMARY = "Rootfs additions for the Stage 1 Rootfs"
-DESCRIPTION = "Rootfs additions for the Stage 1 Rootfs"
-SECTION = "examples"
+SUMMARY = "Synapse Recovery Scripts and Files"
+DESCRIPTION = "Files and scripts necessary to recover from a Synapse Recovery IMG"
+SECTION = "base"
 PR = "r1"
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COREBASE}/LICENSE;md5=4d92cd373abda3937c2bc47fbc49d690 \
                     file://${COREBASE}/meta/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
 
-
 PACKAGES = "${PN}"
-
-#SRC_URI = "file://fw_env.config"
-# Add new SRC_URI += lines here
-
-
+SRC_URI = "file://run-recovery.sh"
 S = "${WORKDIR}"
 
 do_install () {
-	:
-#	mkdir -p ${D}${sysconfdir}
-# Add new mkdirs for your destination path here.  Look at meta/conf/bitbake.conf for built-in variables
+    # directory variables available in meta/conf/bitbake.conf
 
-#	install -D fw_env.config ${D}${sysconfdir}/
-# Add new installation lines here
+    # ensure /usr/bin exists
+	mkdir -p ${D}${bindir}
 
+    # install run-recovery.sh into /usr/bin
+	install -D -m 0755 run-recovery.sh ${D}${bindir}/
 }
 
-#FILES_${PN} = "${sysconfdir}/fw_env.config"
+FILES_${PN} = "${bindir}/run-recovery.sh"
 # Add new Destination files here to be included in the package
 
 # Prevents do_package failures with:
---
+++ recipes-core/synapse-recovery/synapse-recovery/run-recovery.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+STORAGE=/dev/mtd6
+ROOTFS_MTD=/dev/mtd5
+ROOTFS_UBI=core-image-stage2-at91sam9x5ek.ubifs
+KERNEL_MTD=/dev/mtd3
+KERNEL_IMG=uImage-at91sam9x5ek.bin
+UBOOT_BOOTCMD="nboot 0x21000000 0 c0000"
+UBOOT_BOOTARGS="console=ttyS0,115200 mtdparts=atmel_nand:128K(bootstrap)ro,384K(uboot)ro,256K(environment)ro,3328K(uImage)ro,12160K(recovery)ro,208M(rootfs),-(other) ubi.mtd=rootfs root=ubi0:rootfs rw rootfstype=ubifs"
+
+die() {
+    echo $* >&2
+    exit 1
+}
+
+# Verify storage image
+
+# Extract storage image
+nanddump ${STORAGE} | tar xf - -C /run/ \
+    || die "Failed to extract kernel & rootfs from NAND"
+cd /run/ || die "Failed to cd to /run/"
+md5sum -c md5sums || die "Failed to validate md5sums"
+
+# if the storage image contained a rootfs, load it
+if [ -e /run/${ROOTFS_URI} ]; then
+    # Test NAND for bad blocks. Mark them bad if found. Run 5 tests.
+    nandtest -p 5 -m ${ROOTFS_MTD}
+
+    # Don't actually erase because that clears the erase counters
+    # which is necessary to do proper wear leveling
+    #flash_erase ${ROOTFS_MTD} 0 0 || die "Failed to erase ${ROOTFS_MTD}"
+
+    # Create rootfs
+    ubiformat ${ROOTFS_MTD} -y || die "Failed to ubiformat ${ROOTFS_MTD}"
+    ubiattach -p ${ROOTFS_MTD} || die "Failed to ubiattach ${ROOTFS_MTD}"
+    ubimkvol /dev/ubi0 -N rootfs -m || die "Failed to ubimkvol rootfs"
+    ubiupdatevol /dev/ubi0_0 /run/${ROOTFS_UBI} \
+        || die "Failed to write rootfs UBI to NAND"
+fi
+
+# if the storage image contained a rootfs, load it
+if [ -e /run/${KERNEL_IMG} ]; then
+    # Test NAND for bad blocks. Mark them bad if found. Run 5 tests.
+    nandtest -p 5 -m ${KERNEL_MTD}
+
+    # Erase flash which is required for the next steps
+    flash_erase ${KERNEL_MTD} 0 0 || die "Failed to erase ${KERNEL_MTD}"
+
+    # Program in kernel
+    nandwrite -p ${KERNEL_MTD} /run/${KERNEL_IMG} \
+        || die "Failed to write kernel to NAND"
+fi
+
+# Set u-boot params to boot
+fw_setenv bootargs ${UBOOT_BOOTARGS} || die "Failed to set u-boot bootargs"
+fw_setenv bootcmd ${UBOOT_BOOTCMD} || die "Failed to set u-boot bootcmd"
+
+# Reboot
+#reboot
